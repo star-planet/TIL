2e9c2aac67411dc4fd8860c6f91ef07dd8313c23
SeongHyeon Jo <shj2012@naver.com>
2023-11-02 08:17:26 +0900

    [level 2] Title: 오픈채팅방, Time: 144.64 ms, Memory: 39.1 MB -BaekjoonHub

---
+++ 프로그래머스/2/42888. 오픈채팅방/오픈채팅방.cpp
@@ -0,0 +1,42 @@
+#include <string>
+#include <vector>
+#include <map>
+#include <sstream>
+
+using namespace std;
+
+vector<string> solution(vector<string> record) {
+    vector<string> answer, print;
+    map<string, string> user;
+    for(int i=0;i<record.size();i++)
+    {
+        string str[3];
+        string token;
+        stringstream ss(record[i]);
+        int index = 0;
+        
+        while(ss >> token)
+            str[index++] = token;
+        
+        if(str[0] == "Enter")
+        {
+            print.push_back("님이 들어왔습니다.");
+            answer.push_back(str[1]);
+            user[str[1]] = str[2];
+        }
+        else if(str[0] == "Leave")
+        {
+            print.push_back("님이 나갔습니다.");
+            answer.push_back(str[1]);
+        }
+        else
+        {
+            user[str[1]] = str[2];
+        }
+    }
+    for(int i=0;i<answer.size();i++)
+    {
+        answer[i] = user[answer[i]] + print[i];
+    }
+    return answer;
+}
