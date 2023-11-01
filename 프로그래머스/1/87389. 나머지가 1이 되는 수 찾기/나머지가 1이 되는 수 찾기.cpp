b67d602f00a6ce008483bc7f16539ff24977f05d
SeongHyeon Jo <shj2012@naver.com>
2023-11-02 03:53:57 +0900

    [level 1] Title: 나머지가 1이 되는 수 찾기, Time: 0.01 ms, Memory: 4.14 MB -BaekjoonHub

--- 프로그래머스/1/87389. 나머지가 1이 되는 수 찾기/나머지가 1이 되는 수 찾기.cpp
+++ 프로그래머스/1/87389. 나머지가 1이 되는 수 찾기/나머지가 1이 되는 수 찾기.cpp
@@ -4,26 +4,8 @@
 
 using namespace std;
 
-int primeNum(int n)
-{
-    for(int i=2;i<=sqrt(n);i++)
-        if(n%i == 0)
-            return false;
-    return true;
-}
-
 int solution(int n) {
-    int i=1, answer = 0;
-    if (primeNum(n-1)) 
-        answer = n-1;
-    else 
-        while(++i)
-        {
-            if((n-1)%i == 0)
-            {
-                answer = i;
-                break;   
-            }   
-        }
-    return answer;
+    for(int i=2; i<n;++i)
+        if(n%i==1) return i;
+    return n-1;
 }
