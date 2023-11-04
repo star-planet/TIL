053a3315273c029631d37d83cbd2e598f146ed13
SeongHyeon Jo <shj2012@naver.com>
2023-11-04 22:58:13 +0900

    [Bronze III] Title: 직각삼각형, Time: 0 ms, Memory: 1116 KB -BaekjoonHub

--- 백준/Bronze/4153. 직각삼각형/직각삼각형.c
+++ 백준/Bronze/4153. 직각삼각형/직각삼각형.c
@@ -1,25 +1,25 @@
 #include <stdio.h>
-#include <math.h>
 
 int main()
 {
-    int edge[3], temp;
     while (1)
     {
+        int edge[3], max = 0, sum = 0;
         scanf("%d %d %d", &edge[0], &edge[1], &edge[2]);
         if (edge[0] + edge[1] + edge[2] == 0) break;
         for (int i = 0; i < 3; i++)
         {
-            for (int j = i; j < 3; j++)
+            edge[i] *= edge[i];
+            if (max < edge[i]) max = edge[i];
+        }
+        for (int i = 0; i < 3; i++)
+        {
+            if (edge[i] != max)
             {
-                if (edge[j] < edge[i])
-                {
-                    temp = edge[i];
-                    edge[i] = edge[j];
-                    edge[j] = temp;
-                }
+                sum += edge[i];
             }
         }
-        pow(edge[0], 2) + pow(edge[1], 2) == pow(edge[2], 2) ? printf("right\n") : printf("wrong\n");
+        sum == max ? printf("right\n") : printf("wrong\n");
     }
+    return 0;
 }
