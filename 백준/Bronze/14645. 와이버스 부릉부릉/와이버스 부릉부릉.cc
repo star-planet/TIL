e2c7f67a0315898b16690528619c6ff34df4be65
Seonghyeon Jo <shj2012@naver.com>
2023-12-14 22:55:24 +0900

    [Bronze V] Title: 와이버스 부릉부릉, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/14645. 와이버스 부릉부릉/와이버스 부릉부릉.cc
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int N, K, A, B;
+    cin >> N >> K;
+    for(int i=0;i<N;i++)
+    {
+        cin >> A >> B;
+    }
+    cout << "비와이";
+    return 0;
+}
