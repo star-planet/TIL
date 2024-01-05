5dc616dcb42cc991b16fdbebf80b13939911a5f4
Hyeon <shj2012@naver.com>
2024-01-06 03:22:45 +0900

    [Bronze V] Title: 세제곱의 합, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/28701. 세제곱의 합/세제곱의 합.cc
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	int N, Sum = 0;
+	cin >> N;
+	Sum = ((1 + N) * N) / 2;
+	cout << Sum << '\n';
+	cout << Sum * Sum << '\n';
+	cout << Sum * Sum;
+	return 0;
+}
