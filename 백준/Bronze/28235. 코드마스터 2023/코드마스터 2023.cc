158ab4935c7a05656ff18adfaa4e53392309361b
Hyeon <shj2012@naver.com>
2024-01-06 03:17:12 +0900

    [Bronze V] Title: 코드마스터 2023, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

---
+++ 백준/Bronze/28235. 코드마스터 2023/코드마스터 2023.cc
@@ -0,0 +1,25 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	string Cheer;
+	cin >> Cheer;
+	if (Cheer == "SONGDO")
+	{
+		cout << "HIGHSCHOOL";
+	}
+	if (Cheer == "CODE")
+	{
+		cout << "MASTER";
+	}
+	if (Cheer == "2023")
+	{
+		cout << "0611";
+	}
+	if (Cheer == "ALGORITHM")
+	{
+		cout << "CONTEST";
+	}
+	return 0;
+}
