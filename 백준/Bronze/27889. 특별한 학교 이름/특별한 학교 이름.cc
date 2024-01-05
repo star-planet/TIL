8baf798fdc4702be8e134bf98424cb95b5545e0c
Hyeon <shj2012@naver.com>
2024-01-06 03:15:27 +0900

    [Bronze V] Title: 특별한 학교 이름, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

---
+++ 백준/Bronze/27889. 특별한 학교 이름/특별한 학교 이름.cc
@@ -0,0 +1,25 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	string Name;
+	cin >> Name;
+	if (Name == "NLCS")
+	{
+		cout << "North London Collegiate School";
+	}
+	if (Name == "BHA")
+	{
+		cout << "Branksome Hall Asia";
+	}
+	if (Name == "KIS")
+	{
+		cout << "Korea International School";
+	}
+	if (Name == "SJA")
+	{
+		cout << "St. Johnsbury Academy";
+	}
+	return 0;
+}
