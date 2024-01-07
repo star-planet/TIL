367383a1eab597dedd95c84be11d82ee66778380
Hyeon <shj2012@naver.com>
2024-01-07 22:22:17 +0900

    [Bronze II] Title: 손익분기점, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/1712. 손익분기점/손익분기점.cc
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int A, B, C;
+    cin >> A >> B >> C;
+    if ((C - B) <= 0)
+    {
+        cout << "-1";
+    }
+    else
+    {
+        cout << (A / (C - B)) + 1;
+    }
+    return 0;
+}
