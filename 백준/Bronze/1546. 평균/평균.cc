6ff444273e8cf51e5629cabb9f2a6e55380a09a3
Hyeon <shj2012@naver.com>
2024-01-17 17:54:05 +0900

    [Bronze I] Title: 평균, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/1546. 평균/평균.cc
@@ -0,0 +1,29 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	int N;
+	int A[1000];
+	cin >> N;
+
+	for (int i = 0; i < N; i++)
+	{
+		cin >> A[i];
+	}
+
+	long sum = 0;
+	long max = 0;
+
+	for (int i = 0; i < N; i++)
+	{
+		if (A[i] > max)
+		{
+			max = A[i];
+		}
+		sum += A[i];
+	}
+	double result = sum * 100.0 / max / N;
+	cout << result << '\n';
+	return 0;
+}
